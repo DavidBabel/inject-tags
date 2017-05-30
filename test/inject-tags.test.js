c6938f6ee7af2894a8eec936ea3a86dc2d6c9deb
Sven SAULEAU <sven.sauleau@xtuc.fr>
2017-05-30 14:32:15 +0200

    fix: tests

--- test/inject-tags.test.js
+++ test/inject-tags.test.js
@@ -1,19 +1,13 @@
-import jsdom from 'jsdom-global';
-import {assert} from 'chai';
-import injectTag, {removeSpacesBetweenTags} from '../lib/index';
-
-jsdom( undefined, {
-  runScripts: 'dangerously'
-});
+const {assert} = require('chai');
+const {injectTag, removeSpacesBetweenTags} = require('../lib/cjs/index');
 
 describe('inject', () => {
 
   beforeEach(() => {
     document.body.innerHTML = '';
-    document.head.innerHTML = '';
   });
 
-  context('remove space between tags', () => {
+  describe('remove space between tags', () => {
 
     it('should remove space', () => {
       const str = '<a></a>     <b></b>';
@@ -30,12 +24,12 @@ describe('inject', () => {
    * FIXME(sven): JSDom violates spec there
    * https://github.com/tmpvar/jsdom/issues/1802
    */
-  context('noscript tag', () => {
+  describe('noscript tag', () => {
 
     it('should not add the node to the DOM', () => {
       const tag = '<noscript>foobar</noscript>';
 
-      injectTag(tag, document);
+      injectTag(tag, document.body);
 
       assert.lengthOf(document.body.childNodes, 0);
     });
@@ -43,23 +37,23 @@ describe('inject', () => {
     it('should not add the nested img to the DOM', () => {
       const tag = '<noscript><div>foo</div><img /></noscript>';
 
-      injectTag(tag, document);
+      injectTag(tag, document.body);
 
       assert.lengthOf(document.body.childNodes, 0);
     });
   });
 
-  context('script tag', () => {
+  describe('script tag', () => {
 
-    context('inline JavaScript', () => {
+    describe('inline JavaScript', () => {
 
       it('should add the node to the DOM', () => {
         const js = 'Date.now();';
         const tag = `<script>${js}</script>`;
 
-        injectTag(tag, document);
+        injectTag(tag, document.body);
 
-        const [script] = document.head.childNodes;
+        const [script] = document.body.childNodes;
 
         assert.instanceOf(script, HTMLScriptElement);
         assert.equal(script.text, js);
@@ -71,9 +65,9 @@ describe('inject', () => {
           <script>b=2</script>
         `;
 
-        injectTag(tag, document);
+        injectTag(tag, document.body);
 
-        const [script1, script2] = document.head.childNodes;
+        const [script1, script2] = document.body.childNodes;
 
         assert.instanceOf(script1, HTMLScriptElement);
         assert.instanceOf(script2, HTMLScriptElement);
@@ -86,27 +80,29 @@ describe('inject', () => {
         const body = 'foo';
         const tag = `<script>document.body.innerHTML="${body}"</script>`;
 
-        injectTag(tag, document);
+        injectTag(tag, document.body);
 
-        assert.equal(document.body.innerHTML, body);
+        process.nextTick(() => {
+          assert.equal(document.body.innerHTML, body);
+        });
       });
 
       it('should handle execution error of the script', () => {
         const tag = '<script>throw new Error("foo")</script>';
 
-        injectTag(tag, document);
+        injectTag(tag, document.body);
 
         assert.isTrue(true); // shouldn't have thrown an error before
       });
     });
 
-    context('remote ressource', () => {
+    describe('remote ressource', () => {
 
       it('should add the node to the DOM', () => {
         const src = 'http://foo/bar.js';
         const tag = `<script src="${src}"></script>`;
 
-        injectTag(tag, document);
+        injectTag(tag, document.body);
 
         const [script] = document.body.childNodes;
 
@@ -116,12 +112,12 @@ describe('inject', () => {
     });
   });
 
-  context('image tag', () => {
+  describe('image tag', () => {
 
     it('should add the node to the DOM', () => {
       const tag = '<img src="http://a.b/c.png"/>';
 
-      injectTag(tag, document);
+      injectTag(tag, document.body);
 
       const [img] = document.body.childNodes;
 
@@ -132,7 +128,7 @@ describe('inject', () => {
     it('should add the nested nodes to the DOM', () => {
       const tag = '<div><img src="http://a.b/c.png"/></div>';
 
-      injectTag(tag, document);
+      injectTag(tag, document.body);
 
       const [div] = document.body.childNodes;
       const [img] = div.childNodes;
@@ -141,12 +137,12 @@ describe('inject', () => {
     });
   });
 
-  context('div', () => {
+  describe('div', () => {
 
     it('should add the node to the DOM', () => {
       const tag = '<div>foo</div>';
 
-      injectTag(tag, document);
+      injectTag(tag, document.body);
 
       const [div] = document.body.childNodes;
 
